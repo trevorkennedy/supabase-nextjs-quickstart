f84e5830bf3aca9b416c349fbd380ea55e861c13
Trevor <direct@trevorkennedy.com>
2023-04-16 12:49:19 -0500

    starter stuff

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Hero from '@components/hero'
+import Banner from '@components/banner'
+import Card from '@components/card'
+import { supabase } from 'lib/supabase'
 
-export default function Home() {
+export const revalidate = 60
+
+export async function getServerSideProps() {
+  let { data } = await supabase.from('posts').select()
+  return {
+    props: {
+      posts: data
+    },
+  }
+}
+
+export default function Home({ posts }) {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Banner />
+      <Hero />
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {posts?.map((item) => (
+          <Card 
+            key={item.id}
+            title={item.title}
+            link={item.link}
+            text={item.content} />
+        ))}
+      </div>
+    </main>
     </div>
   )
 }
