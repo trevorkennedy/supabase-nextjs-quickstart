116fbda9e556a9efcf1255ce8067528284c498d8
Trevor <direct@trevorkennedy.com>
2023-04-16 14:57:53 -0500

    With Supabase data
    Why was this so painful?

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,32 @@
 import Hero from '@components/hero'
 import Banner from '@components/banner'
 import Card from '@components/card'
+import { supabase } from 'lib/supabase'
+
 export const revalidate = 60
 
-export default function Home() {
+export async function getServerSideProps() {
+  let { data } = await supabase.from('posts').select()
+  return {
+    props: {
+      posts: data
+    },
+  }
+}
+
+export default function Home({posts}) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Banner />
       <Hero />
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-      <Card 
-            key="{item.id}"
-            title="{item.title}"
-            link="{item.link}"
-            text="{item.content}" />
+        {posts.map((item) => (
+          <Card 
+            key={item.id}
+            title={item.title}
+            link={item.link}
+            text={item.content} />
+        ))}
       </div>
     </main>
   )
